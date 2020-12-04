18c52feded3c13c8562083205bc8526b138b61dc
AntonHuggard <56659693+AntonHuggard@users.noreply.github.com>
2020-12-05 11:50:54 +1300

    Init commit - Larr_v4

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Anton Huggard, May 2020 -- jQuery has been added
+
+
+$(document).ready(function(){
+    $("#hamburger").click(function(){
+      var mobile_menu = $(".mobile_top_menu");
+      mobile_menu.toggleClass("hidden");
+    });
+});
+  
+function scam_function() {
+    alert('this page has stopped responding');
+    alert('well would you look at that. It\'s working again. What are the odds?')
+}
+function goto_page(link) {
+    window.location.href = link;
+}
+
+
+function colourChange() {
+    document.getElementById("normal_head").style.backgroundColor = "black";
+}
+
+function random() {
+    let num = Math.floor((Math.random() * 37) + 1);
+    window.location='../'+num;
+}
+
+let monochrome = true;
+function toggle_colour() {
+    if (monochrome) {
+        document.getElementById("comic").src = "../assets/img/comics/12_19e-C.png";
+        monochrome = false;
+    } else {
+        document.getElementById("comic").src = "../assets/img/comics/12_19e.png";
+        monochrome = true;
+    }
+}
